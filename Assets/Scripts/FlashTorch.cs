2cd810fdec359703445429192c7a15c25f071f81
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-11-15 18:55:15 -0300

    Implemented flash torch button

---
+++ Assets/Scripts/FlashTorch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class FlashTorch : MonoBehaviour {
+
+    public bool linterna = false;
+
+    private void Start()
+    {
+        // Desactiva la linterna al comenzar
+        CameraDevice.Instance.SetFlashTorchMode(false);
+    }
+
+    // Activa o desactiva la linterna
+    public void ActivarDesactivarLinterna()
+    {
+        linterna = !linterna;
+        CameraDevice.Instance.SetFlashTorchMode(linterna);
+    }
+}
